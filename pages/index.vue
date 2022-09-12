c0ddaf90e63210820b5e3728665ada904712a814
ARMANDO BOARETTO <111752771+ArmandoBoareT@users.noreply.github.com>
2022-09-12 11:32:17 +0000

    aula

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,80 @@
 <template>
+  
   <section class="section">
     <h1 class="title">Página inicial</h1>
+  <b-carousel>
+      <b-carousel-item>
+        <b-image class="image" src="https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2022/08/WhatsApp-Image-2022-08-02-at-15.26.19-2.jpeg?w=876&h=484&crop=1"></b-image>
+      </b-carousel-item>
+      <b-carousel-item>
+        <b-image class="image" src="https://picsum.photos/id/432/1230/300"></b-image>
+      </b-carousel-item>      
+      <b-carousel-item>
+        <b-image class="image" src="https://picsum.photos/id/433/1230/300"></b-image>
+      </b-carousel-item>            
+    </b-carousel>
+    <br>
+     <h1 class="title">Mais vendidos</h1>
+    <div class="columns mt-4">
+      <div class="column is-3">
+        <article class="notification is-primary has-text-centered">
+          <p class="title">Toalha do Lula</p>
+          <b-image class="image" src="https://cf.shopee.com.br/file/14fe36d68cce9da6e5501619626dd993">          </b-image>
+          <br>
+          <template>
+    <section>
+        <b-button click="clickMe"> Comprar</b-button>
+    </section>
+</template>
+          <br>
+          
+          <p class="subtitle"><strong>R$125,00</strong></p>
+        </article>
+      </div>
+      <div class="column is-3">
+        <article class="notification is-info has-text-centered">
+          <p class="title">Toalha do Bolsonaro</p>
+          <b-image class="image" src="https://cf.shopee.com.br/file/213ba50a7b0f3516a4a4e67ad95270dd">          </b-image>
+          <br>
+          <b-button click="clickMe"> Comprar</b-button>
+          <br>
+          <br>
+          <p class="subtitle"><strong>R$125,00</strong></p>
+        </article>
+      </div>
+      <div class="column is-3">
+        <article class="notification is-primary has-text-centered">
+          <p class="title">Toalha do Moro</p>
+          <b-image class="image" src="https://http2.mlstatic.com/D_NQ_NP_906863-MLB46185146779_052021-O.jpg">          </b-image>
+          <br>
+          <b-button click="clickMe"> Comprar</b-button>
+          <br>
+          <br>
+          <p class="subtitle"><strong>R$125,00</strong></p>
+        </article>
+      </div>
+      <div class="column is-3">
+        <article class="notification is-info has-text-centered">
+          <p class="title">Toalha do Doria</p>
+          <b-image class="image" src="https://http2.mlstatic.com/D_NQ_NP_633411-MLB49838713756_052022-V.jpg">          </b-image>
+          <br>
+          <b-button click="clickMe"> Comprar</b-button>
+          <br>
+          <br>
+          <p class="subtitle">R$125,00</p>
+        </article>
+      </div>
+      </div>
+     <div class="columns mt-4">
+      <div class="column is">
+        <article class="notification is-primary has-text-centered">
+          <p class="title">Bem vindo a 2022</p>
+          <p class="subtitle">O Ano em que as pessoas irão comprar toalhas</p>
+        </article>
+      </div>
+    </div>
+  
   </section>
+
 </template>
 
